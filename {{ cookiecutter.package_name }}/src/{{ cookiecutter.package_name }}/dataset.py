b6dce356248c35fd892749a2cf6840896d083f0e
Henry Stern <108289013+henrystern@users.noreply.github.com>
2025-04-27 20:15:51 -0400

    protect log file from autoreload

--- {{ cookiecutter.package_name }}/src/{{ cookiecutter.package_name }}/dataset.py
+++ {{ cookiecutter.package_name }}/src/{{ cookiecutter.package_name }}/dataset.py
@@ -3,4 +3,5 @@
 from {{ cookiecutter.package_name }}.config import PROCESSED_DATA_DIR, RAW_DATA_DIR, LOGS_DIR
 from loguru import logger
 
-logger.add(LOGS_DIR / "dataset.py") # Log to file to monitor dataset updates.
+if __name__ == "__main__":
+    logger.add(LOGS_DIR / "dataset.py") # Log to file to monitor dataset updates.
